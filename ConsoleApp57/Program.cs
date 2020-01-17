98345bc2265a79742049c1f27c82b4ff76014a6a
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-17 15:52:42 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp57/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, tmp;
+            Console.Write("How many elements will the array have? (1-1000): n=");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] tab1 = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Tell me tab1[{0}]", i);
+                tab1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i=0; i<n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (tab1[i] < tab1[j])
+                    {
+                        tmp = tab1[i]; tab1[i] = tab1[j]; tab1[j] = tmp;
+                    }
+                }
+            }
+            foreach (int elem in tab1) Console.WriteLine("Array elemet: {0}", elem);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp57/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("ConsoleApp57")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp57")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("b98a1c21-d7a8-4bba-b9b9-f192ab6e2e04")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
